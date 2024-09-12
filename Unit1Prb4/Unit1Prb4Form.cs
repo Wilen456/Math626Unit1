1275d9a5bd1bb275be7ce5fb64222d8e968b1504
Wilen456 <gpoelling1@gmail.com>
2024-09-11 23:13:35 -0500

    completed problem5, works as intended

--- Unit1Prb4/Unit1Prb4Form.cs
+++ Unit1Prb4/Unit1Prb4Form.cs
@@ -4,7 +4,7 @@
 /*                                                          */
 /*  Program: Unit1Prb4.cs                                   */
 /*                                                          */
-/*  Programmer:                                             */
+/*  Programmer: Griffin                                     */
 /*                                                          */
 /*  Purpose: Test ArrayQueueLib with simple work order      */
 /*           application                                    */
@@ -14,9 +14,130 @@ using ArrayQueueLib;
 
 namespace Unit1Prb4
 {
+    public class ArrayQueue
+    {
+        private string name;
+        private int size;
+        private object?[] array;
+        private const int INCREMENT = 10;
+        private int front;
+        private int rear;
+
+        public ArrayQueue()
+        {
+            name = "DefaultQueue";
+            size = 0;
+            array = new object[INCREMENT];
+            front = 0;
+            rear = 0;
+        }
+        public ArrayQueue(string queueName)
+        {
+            name = queueName;
+            size = 0;
+            array = new object[INCREMENT];
+            front = 0;
+            rear = 0;
+        }
+
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int Front
+        {
+            get { return front; }
+        }
+
+        public int Size
+        {
+            get { return size; }
+        }
+
+
+        public object[] List
+        {
+            get { return array; }
+        }
+
+
+        public bool Empty
+        {
+            get { return size == 0; }
+        }
+
+
+        public void Purge()
+        {
+            array = new object[INCREMENT]; // Reset the array
+            size = 0;
+            front = 0;
+            rear = 0;
+        }
+        public int Count
+        {
+            get { return size; }
+        }
+
+
+        public void Enqueue(object data)
+        {
+            if (size == array.Length)
+            {
+                ResizeArray(); // Resize if full
+            }
+
+            array[rear] = data;
+            rear = (rear + 1) % array.Length; 
+            size++;
+
+        }
+
+
+        public object Dequeue()
+        {
+            if (Empty)
+            {
+                throw new InvalidOperationException("Queue is empty.");
+            }
+
+            object dequeuedData = array[front];
+            array[front] = null;
+            front = (front + 1) % array.Length; // Move the front forward
+            size--;
+            return dequeuedData;
+        }
+        public object Peek()
+        {
+            if (Empty)
+            {
+                throw new InvalidOperationException("Queue is empty.");
+            }
+
+            return array[front]; // Return the front element without removing it
+        }
+
+
+        private void ResizeArray()
+        {
+            object[] newArray = new object[array.Length + INCREMENT];
+            for (int i = 0; i < size; i++)
+            {
+                newArray[i] = array[(front + i) % array.Length]; // Copy elements in the correct order
+            }
+
+            array = newArray;
+            front = 0;
+            rear = size;
+        }
+
+
+    }
     public partial class Unit1Prb4Form : Form
     {
-       //  public ArrayQueue orderQueue = new ArrayQueue("Work order queue");
+        public ArrayQueue orderQueue = new ArrayQueue("Work order queue");
         public Unit1Prb4Form()
         {
             InitializeComponent();
@@ -24,7 +145,7 @@ namespace Unit1Prb4
 
         private void newJobButton_Click(object sender, EventArgs e)
         {
-            /*
+
             Order newOrder = new Order();
             addForm addDialog = new addForm();
             ;
@@ -35,14 +156,15 @@ namespace Unit1Prb4
                 newOrder.Name = addDialog.returnName;
 
                 orderQueue.Enqueue(newOrder);
+
             }
 
             PrintQueue();
-            */
+
         }
         private void releaseOrderButton_Click(object sender, EventArgs e)
         {
-            /*
+
             if (!orderQueue.Empty)
             {
                 Order releasedOrder;
@@ -54,52 +176,64 @@ namespace Unit1Prb4
             {
                 MessageBox.Show("Work order queue is empty", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
-            */
+
 
         }
         private void PrintQueue()
         {
-           // Missing code
-        }
-    }
-    public class Order
-    {
-        private int orderNumber;
-        private string name;
+            numTextBox.Text = orderQueue.Count.ToString();
+            queueTextBox.Clear();
+            object[] queueItems = orderQueue.List;
 
-        public Order()
-        {
-            this.orderNumber = 0; this.name = "";
-        }
-
-        public int OrderNumber // Define read/write Order Number property
-        {
-            get
+            for (int i = 0; i < orderQueue.Size; i++)
             {
-                return this.orderNumber;
-            }
-
-            set
-            {
-                this.orderNumber = value;
+                int index = (orderQueue.Front + i) % queueItems.Length;
+                Order order = (Order)queueItems[index];
+                if (order != null)
+                {
+                    queueTextBox.AppendText(order.ToString() + Environment.NewLine);
+                }
             }
         }
-
-        public string Name // Define read/write UserName property
+        public class Order
         {
-            get
+            private int orderNumber;
+            private string name;
+
+            public Order()
             {
-                return this.name;
+                this.orderNumber = 0; this.name = "";
             }
 
-            set
+            public int OrderNumber // Define read/write Order Number property
             {
-                this.name = value;
+                get
+                {
+                    return this.orderNumber;
+                }
+
+                set
+                {
+                    this.orderNumber = value;
+                }
             }
-        }
-        public override string ToString()
-        {
-            return this.orderNumber.ToString() + ", " + this.name;
+
+            public string Name // Define read/write UserName property
+            {
+                get
+                {
+                    return this.name;
+                }
+
+                set
+                {
+                    this.name = value;
+                }
+            }
+            public override string ToString()
+            {
+                return this.orderNumber.ToString() + ", " + this.name;
+            }
         }
     }
 }
--- Unit1Prb5/Unit1Prb5Form.cs
+++ Unit1Prb5/Unit1Prb5Form.cs
@@ -4,7 +4,7 @@
 /*                                                          */
 /*  Program: Unit1Prb5.cs                                   */
 /*                                                          */
-/*  Programmer:                                             */
+/*  Programmer: Griffin Poelling                            */
 /*                                                          */
 /*  Purpose: Test LinkedQueueLib with simple job queue      */
 /*           application                                    */
@@ -14,10 +14,100 @@ using LinkedQueueLib;
 
 namespace Unit1Prb5
 {
+    public class LinkedQueue
+    {
+        private QueueNode head;
+        private QueueNode tail;
+        private int size;
+        private string Name;
+        
+        public LinkedQueue()
+        {
+            head = null;
+            tail = null;
+            size = 0;
+            Name = string.Empty;
+        }
+        public LinkedQueue(string name)
+        {
+            head = null;
+            tail = null;
+            size = 0;
+            Name = name;
+        }
+        public string name()
+        {
+            return Name;
+        }
+        public int Size()
+        {
+            return size;
+        }
+        public bool Empty()
+        {
+            if (size == 0)
+            {
+                return true;
+            }
+            else return false;
+        }
+        public void Enqueue(object data)
+        {
+            QueueNode newNode = new QueueNode(data);
+            if (this.Empty())
+            {
+                head = newNode;
+                tail = newNode;
+            }
+            else
+            {
+                tail.Next = newNode;
+                tail = newNode;
+            }
+            size++;
+        }
+        public object Dequeue()
+        {
+            if (this.Empty())
+                throw new InvalidOperationException("Queue is empty.");
 
+            object data = head.Data;
+            head = head.Next;
+            size--;
+
+            // If the queue is empty, reset the tail as well
+            if (this.Empty())
+                tail = null;
+
+            return data;
+        }
+        public QueueNode Peek()
+        {
+            if (this.Empty())
+                return null;
+            return head;
+        }
+        public void Purge()
+        {
+            head = null;
+            tail = null;
+            size = 0;
+        }
+    }
+
+    public class QueueNode
+    {
+        public object Data { get; set; }
+        public QueueNode Next { get; set; }
+        public QueueNode(object data)
+        {
+            Data= data;
+            Next = null;
+        }
+    }
     public partial class Unit1Prb5Form : Form
     {
-        // public LinkedQueue jobQueue = new LinkedQueue("Job Queue");
+         public LinkedQueue jobQueue = new LinkedQueue("Job Queue");
         public Unit1Prb5Form()
         {
             InitializeComponent();
@@ -25,7 +115,7 @@ namespace Unit1Prb5
 
         private void newJobButton_Click(object sender, EventArgs e)
         {
-            /*
+            
             Job newJob = new Job();
             addForm addDialog = new addForm();
             ;
@@ -39,13 +129,13 @@ namespace Unit1Prb5
             }
 
             PrintQueue();
-            */
+            
         }
 
         private void releaseJobButton_Click(object sender, EventArgs e)
         {
-            /*
-            if (!jobQueue.Empty)
+            
+            if (!jobQueue.Empty())
             {
                 Job releasedJob;
                 releasedJob = (Job)jobQueue.Dequeue();
@@ -56,16 +146,16 @@ namespace Unit1Prb5
             {
                 MessageBox.Show("Job queue is empty", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
-            */
+            
 
         }
         private void PrintQueue()
         {
-            /*
+            
             QueueNode currentNode;
             Job currentJob;
 
-            numTextBox.Text = jobQueue.Size.ToString();
+            numTextBox.Text = jobQueue.Size().ToString();
 
             queueTextBox.Clear();
 
@@ -78,7 +168,7 @@ namespace Unit1Prb5
                 queueTextBox.Text += System.Environment.NewLine;
                 currentNode = currentNode.Next;
             }
-            */
+            
         }
 
     }
